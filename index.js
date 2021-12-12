1cfed8dbb4bb5901b13416b21f5f82f64eff8b2f
Omri <omrisig1@gmail.com>
2021-12-12 02:42:50 -0800

    before typescript

--- index.js
+++ index.js
@@ -29,12 +29,17 @@ async function start() {
             await writeToFile(filename, tasks);
             break;
         case ('read'):
-            readTasks(filename);
+            readTasks(filename, myArgs[1]);
             break;    
         case ('delete'):
             content = await readTasksFile(filename);
             content = deleteTask(content, myArgs[1]);
             await writeToFile(filename, content);
+            break;
+        case ('remove-comepleted'):
+             content = await readTasksFile(filename);
+             content = removeCompleted(content);
+             await writeToFile(filename, content);
             break;
         case ('help'):
         default:
@@ -59,10 +64,29 @@ function updateTask(tasklist, taskname){
     return tasklist;
 }
 
-async function readTasks(filename){
+async function readTasks(filename, filter = null){
+    console.log(filter);
     let content = await readTasksFile(filename);
+    switch(filter){
+        case('completed'):
+            content = content.filter((element)=>element.done == true);
+            break;
+        case('open'):
+            content = content.filter((element)=>element.done == false);
+            break
+        case('all'):
+        default:
+            break;
+    }
     console.log(content);
+
 }
+
+function removeCompleted(content){
+    content = content.filter(element => element.done == false);
+    return content;
+}
+
 
 function deleteTask(content, taskname){
     const foundIndex = content.findIndex(element => element.name == taskname);
@@ -74,9 +98,11 @@ function deleteTask(content, taskname){
 }
 
 function showHelp(){
-    console.log('help menu:\n your options are:\n \"create\" and task name\n \"update\" and task name in order to set to completed\n \"read\" to see all tasks\n \"delete\" and task name to delete a specific task\n \"help\" to see your options\n');
-}
+    console.log('help menu:\n your options are:\n \"create\" and task name\n \"update\" and task name in order to set to completed\n \"read all\" to see all tasks, all is the default\n \"read completed\" to see commpleted tasks\n \"read open\" to see open tasks\n \"delete\" and task name to delete a specific task\n \"remove-completed\" to remove completed tasks from list\n \"help\" to see your options\n');
 
+
+}
+//Add parameters to display either all tasks, completed tasks or open tasks
 async function createFileIfNotExists(filename){
     if ( fs.existsSync(filename)) {
         return 1;
@@ -102,7 +128,7 @@ async function readTasksFile(filename) {
 
 async function writeToFile(filename, content) {
     await createFileIfNotExists(filename);
-    fs.writeFile('tasks.json', JSON.stringify(content), err => {
+    fs.writeFile('tasks.json', JSON.stringify(content,null,4), err => {
         if (err) {
           console.error(err)
           return 0;
